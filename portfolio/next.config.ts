8a9191d865860552759f10c64a8e25881fb5cc0a
Ian <iangonsalves29@gmail.com>
2025-07-14 00:30:07 +0400

    Updates config

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  assetPrefix: '/portfolio-website/',
+  basePath: isProd ? '/portfolio-website' : '',
+  assetPrefix: isProd ? '/portfolio-website/' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
