3560b6fd87933dd4de9f581e081532f7e1e1516f
Ian <iangonsalves29@gmail.com>
2025-07-14 13:26:19 +0400

    updated fsvicon

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon-v2.ico" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
